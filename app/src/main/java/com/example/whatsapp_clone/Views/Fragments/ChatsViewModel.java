e97cd7cf1031e970e11ff3bd93f1f8e14af48974
saar azari <51089069+saarazari5@users.noreply.github.com>
2023-06-12 17:53:49 +0300

    projectStructure

---
+++ app/src/main/java/com/example/whatsapp_clone/Views/Fragments/ChatsViewModel.java
@@ -0,0 +1,7 @@
+package com.example.whatsapp_clone.Views.Fragments;
+
+import androidx.lifecycle.ViewModel;
+
+public class ChatsViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/example/whatsapp_clone/Views/Fragments/LoginViewModel.java
@@ -0,0 +1,7 @@
+package com.example.whatsapp_clone.Views.Fragments;
+
+import androidx.lifecycle.ViewModel;
+
+public class LoginViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
